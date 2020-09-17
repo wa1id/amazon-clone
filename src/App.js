7753c94b2ec3485fc19b3aa21bb6a644df632820
walid <wyachou95@gmail.com>
2020-09-17 20:06:01 +0200

    add styling for header and logo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Header from "./components/header/Header";
 function App() {
   return (
     <div className="app">
-      <h1>Working</h1>
-      {/* Header */}
       <Header />
-      {/* Home */}
     </div>
   );
 }
