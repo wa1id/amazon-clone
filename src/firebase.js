ed1e1fb200824351f5852fe044efc821afa1f7bc
walid <wyachou95@gmail.com>
2020-09-17 19:02:58 +0200

    Basic react app with firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDmhWCWhjnIbErTmWxFaMG2ixgCB7FuamA",
+  authDomain: "clone-7c37d.firebaseapp.com",
+  databaseURL: "https://clone-7c37d.firebaseio.com",
+  projectId: "clone-7c37d",
+  storageBucket: "clone-7c37d.appspot.com",
+  messagingSenderId: "805960309105",
+  appId: "1:805960309105:web:e4bc645559b142b07f7ed0",
+};
