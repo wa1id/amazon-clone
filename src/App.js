d0cd50bfe795e3f9df19e38e65eb684475746510
walid <wyachou95@gmail.com>
2020-09-18 00:12:00 +0200

    subtotal style and currencyformat

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/header/Header";
 import Home from "./components/Home";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Checkout from "./components/Checkout";
+import Checkout from "./components/checkout/Checkout";
 
 function App() {
   return (
--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -17,7 +18,7 @@ function Checkout() {
       </div>
 
       <div className="checkout__right">
-        <h2>subtotal</h2>
+        <Subtotal />
       </div>
     </div>
   );
