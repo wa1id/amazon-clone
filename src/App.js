4ed2deb43d77111e0682085a662cea6c25ef9fdb
walid <wyachou95@gmail.com>
2020-09-17 22:11:46 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./Home.css";
 import Product from "./Product";
 
