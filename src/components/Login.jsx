0d40b8920f6e96eb711d801b8a0e630a50965c7c
walid <wyachou95@gmail.com>
2020-09-18 20:59:29 +0200

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,6 +10,13 @@ function Login() {
 
   const signIn = (e) => {
     e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("");
+      })
+      .catch((error) => alert(error.message));
   };
 
   const register = (e) => {
